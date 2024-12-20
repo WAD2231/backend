e9103d87cfbe8f7d6727fd2bc469311d6542082f
22120143 <22120143@student.hcmus.edu.vn>
2024-12-20 21:18:41 +0700

    Update auth route

--- controllers/auth.c.js
+++ controllers/auth.c.js
@@ -1,39 +1,6 @@
-const User = require('../models/user.m.js');
 const passport = require('passport');
-const bcrypt = require('bcrypt');
-const USER = process.env.PERMISSION_USER;
-const ADMIN = process.env.PERMISSION_ADMIN;
 
 module.exports = {
-    register: async (req, res) => {
-        try {
-            // Check if username already exists
-            const existedUsername = await User.getUserByUsername(req.body.username);
-            if (existedUsername) {
-                return res.status(400).send('Username already exists');
-            }
-
-            // Check if email already exists
-            const existedEmail = await User.getUserByEmail(req.body.email);
-            if (existedEmail) {
-                return res.status(400).send('Email already exists');
-            }
-
-            // Encrypt password
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-            // Create user
-            const user = req.body;
-            user.password = hashedPassword;
-            const user_id = await User.createUser(user);
-            return res.status(201).json({ user_id });
-        }
-        catch (error) {
-            return res.status(500).send(error);
-        }
-    },
-
     localLogin: async (req, res, next) => {
         passport.authenticate('local', (err, user, info) => {
             if (err) {
@@ -49,7 +16,7 @@ module.exports = {
                     return next(err);
                 }
 
-                return res.status(200).json({ user_id: user.user_id });
+                return res.status(200).json({ message: 'Logged in' });
             });
         })(req, res, next);
     },
@@ -82,7 +49,7 @@ module.exports = {
                     return next(err);
                 }
 
-                return res.status(200).json({ user_id: user.user_id });
+                return res.status(200).json({ message: 'Logged in'});
             });
         })(req, res, next);
     },
@@ -106,7 +73,7 @@ module.exports = {
                     return next(err);
                 }
 
-                return res.status(200).json({ user_id: user.user_id });
+                return res.status(200).json({ message: 'Logged in' });
             });
         })(req, res, next);
     }
