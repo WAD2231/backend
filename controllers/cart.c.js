90a11f60994765b54ffa3ae922dec3d625ba60d8
22120143 <22120143@student.hcmus.edu.vn>
2024-12-26 00:05:29 +0700

    Create CartItem model and fix CartController

--- controllers/cart.c.js
+++ controllers/cart.c.js
@@ -1,4 +1,5 @@
 const Cart = require('../models/cart.m.js');
+const CartItem = require('../models/cart-item.m.js');
 
 module.exports = {
     createCart: async (req, res) => {
@@ -15,7 +16,7 @@ module.exports = {
     getItems: async (req, res) => {
         try {
             const userID = req.user.user_id;
-            const items = await Cart.getItems(userID);
+            const items = await CartItem.getItems(userID);
             res.status(200).json(items);
         }
         catch (error) {
@@ -28,8 +29,15 @@ module.exports = {
             const userID = req.user.user_id;
             const productID = req.body.product_id;
             const cartID = await Cart.getCart(userID);
-            const item = await Cart.addItem(cartID, productID);
-            res.status(201).json(item);
+            const existingItem = await CartItem.getItem(cartID, productID);
+            if (existingItem) {
+                CartItem.updateItem(cartID, productID, existingItem.quantity + 1);
+                return res.status(200).json(existingItem);
+            }
+            else {
+                const item = await CartItem.addItem(cartID, productID);
+                res.status(201).json(item);
+            }
         }
         catch (error) {
             res.status(500).send('An error occurred while adding the item');
@@ -41,7 +49,7 @@ module.exports = {
             const userID = req.user.user_id;
             const itemID = req.params.id;
             const cartID = await Cart.getCart(userID);
-            await Cart.deleteItem(cartID, itemID);
+            await CartItem.deleteItem(cartID, itemID);
             res.status(204).send();
         }
         catch (error) {
@@ -55,7 +63,7 @@ module.exports = {
             const itemID = req.params.id;
             const quantity = req.body.quantity;
             const cartID = await Cart.getCart(userID);
-            const item = await Cart.updateItem(cartID, itemID, quantity);
+            const item = await CartItem.updateItem(cartID, itemID, quantity);
             res.status(200).json(item);
         }
         catch (error) {
