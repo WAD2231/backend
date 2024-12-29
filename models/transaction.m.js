88e75a4ac5091c073f28da5187ca662e5abdad7e
22120143 <22120143@student.hcmus.edu.vn>
2024-12-29 23:14:23 +0700

    Fix createTransaction function

--- models/transaction.m.js
+++ models/transaction.m.js
@@ -4,33 +4,38 @@ const mainID = 1;
 
 module.exports = {
     createTransaction: async (accountId, amount) => {
-        try {
-            const transactionID = await t.one(`
-                INSERT INTO ${SCHEMA}.transactions (account_id, amount)
-                VALUES ($1, $2)
-                RETURNING transaction_id
-            `, [accountId, amount]);
-        
-            await t.none(`
-                UPDATE ${SCHEMA}.accounts
-                SET balance = balance + $1
-                WHERE account_id = $2
-            `, [amount, mainID]);
-        
-            await t.none(`
-                UPDATE ${SCHEMA}.accounts
-                SET balance = balance - $1
-                WHERE account_id = $2
-            `, [amount, accountId]);
+        db.tx(async t => {
+            try {
+                const transactionID = await t.one(`
+                    INSERT INTO ${SCHEMA}.transactions (account_id, amount)
+                    VALUES ($1, $2)
+                    RETURNING transaction_id
+                `, [accountId, amount]);
+            
+                await t.none(`
+                    UPDATE ${SCHEMA}.accounts
+                    SET balance = balance + $1
+                    WHERE account_id = $2
+                `, [amount, mainID]);
+            
+                await t.none(`
+                    UPDATE ${SCHEMA}.accounts
+                    SET balance = balance - $1
+                    WHERE account_id = $2
+                `, [amount, accountId]);
 
-            await t.none(`
-                UPDATE ${SCHEMA}.transactions
-                SET status = 'completed'
-                WHERE transaction_id = $1    
-            `, [transactionID.transaction_id]);        
-            } 
-        catch(err) {
-            throw err;
-        }     
+                await t.none(`
+                    UPDATE ${SCHEMA}.transactions
+                    SET status = 'completed'
+                    WHERE transaction_id = $1    
+                `, [transactionID.transaction_id]);        
+            
+            } catch (error) {
+                throw error; 
+            }
+        })
+        .catch(error => {
+            throw error;
+        });       
     },
 };
