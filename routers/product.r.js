e3e4e6591f0fa93f75c2d1554f3569f5617a2651
PhamGiaKhiem <pgk06052018@gmail.com>
2025-01-09 23:36:28 +0700

    some fix in route

--- routers/product.r.js
+++ routers/product.r.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/product.c.js');
-const upload = require('../middlewares/upload');
+const multer = require("multer");
+const uploadSingle = require("../middlewares/uploadSingle");
+const uploadArray = require("../middlewares/uploadArray");
+const { verifyAdmin } = require("../middlewares/authorize.js");
+const upload = multer();
 
 // Route to get filtered products with pagination
 router.get('/', productController.getProducts);
@@ -10,10 +14,10 @@ router.get('/', productController.getProducts);
 router.get('/details', productController.getProductDetails);
 
 // Route to create a new product
-router.post('/create', upload.single('image'), productController.createProduct);
+router.post('/create', verifyAdmin, upload.array('image', 10), uploadArray, productController.createProduct);
 
 // Route to update an existing product by ID
-router.put('/:id', upload.single('image'), productController.updateProduct);
+router.put('/:id', verifyAdmin, upload.single('image'), uploadSingle, productController.updateProduct);
 
 // Route to delete a product by ID
 router.delete('/:id', productController.deleteProduct);
