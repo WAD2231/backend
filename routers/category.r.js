0e0e8001f428dff97a2ff16fb30b815fd0258c95
PhamGiaKhiem <pgk06052018@gmail.com>
2025-01-09 23:18:32 +0700

    fix product

--- routers/category.r.js
+++ routers/category.r.js
@@ -1,23 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const categoryController = require('../controllers/category.c.js');
+const categoryController = require("../controllers/category.c.js");
+const multer = require("multer");
+const uploadSingle = require("../middlewares/uploadSingle");
+const { verifyAdmin } = require("../middlewares/authorize.js");
+const upload = multer();
 
 // Route to get all categories
-router.get('/', categoryController.getCategories);
+router.get("/", categoryController.getCategories);
 
 // Route to get category details by ID
-router.get('/:id', categoryController.getCategoryDetail);
+router.get("/:id", verifyAdmin, categoryController.getCategoryDetail);
 
 // Route to create a new category
-router.post('/', categoryController.createCategory);
+router.post(
+  "/",
+  verifyAdmin,
+  upload.single("thumbnail"),
+  uploadSingle,
+  categoryController.createCategory
+);
 
 // Route to update an existing category by ID
-router.put('/:id', categoryController.updateCategory);
+router.put(
+  "/:id",
+  verifyAdmin,
+  upload.single("thumbnail_url"),
+  uploadSingle,
+  categoryController.updateCategory
+);
 
 // Route to delete a category by ID
-router.delete('/:id', categoryController.deleteCategory);
+router.delete("/:id", verifyAdmin, categoryController.deleteCategory);
 
 // Route to get products in a specific category
-router.get('/products/:field/:value', categoryController.getProductsInCategory);
+router.get("/products/:id", categoryController.getProductsInCategory);
 
 module.exports = router;
