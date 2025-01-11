5d6ce48ae4ceccadaed069127851ac6242f56d5c
22120143 <22120143@student.hcmus.edu.vn>
2025-01-11 08:07:42 +0700

    Init account for admin in subsystem

--- models/account.m.js
+++ models/account.m.js
@@ -1,6 +1,29 @@
 const db = require('./db');
 const SCHEMA = process.env.DB_SCHEMA;
 
+const init = async () => {
+    try {
+        const accountCount = await db.one(`
+            SELECT COUNT(*) AS count
+            FROM ${SCHEMA}.accounts    
+        `);
+
+        if (accountCount.count > 0) {
+            return;
+        }
+
+        await db.none(`
+            INSERT INTO ${SCHEMA}.accounts (balance)
+            VALUES (0)
+        `);
+    }
+    catch (error) {
+        throw error;
+    }
+};
+
+init();
+
 module.exports = {
     createAccount: async () => {
         try {
